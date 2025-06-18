0ac4ecc955b5eb3d58132219873b8986aa71ab86
cookiebanner2025 <analyticsmaster2@gmail.com>
2025-06-18 15:34:39 +0600

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -2196,6 +2196,7 @@ function injectConsentHTML(detectedCookies, language = 'en') {
         background-color: ${config.buttonStyle.reject.background};
         color: ${config.buttonStyle.reject.color};
         border: ${config.buttonStyle.reject.border};
+        display: none;
         
     }
 
